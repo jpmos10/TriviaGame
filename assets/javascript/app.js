f6453d4a4195eba984d032e5fd05bd6c6933b5e8
Jason Mosley <jpmos10@users.noreply.github.com>
2016-09-02 19:53:52 -0500

    adding question structure in add.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+var Questions = [
+
+	1 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+
+	2 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+	    
+	3 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    }, 
+	    
+	4 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+	    
+	5 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+	    
+	6 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },  
+
+	7 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+	    
+	 8 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+	    
+	 9 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },
+
+	 10 : {
+		question : 'Ask Question Here?';
+		choice1 : 'text';
+		choice2 : 'text';
+		choice3 : 'text';
+		choice4 : 'text';
+	    },                            
+
+
+
+
+
+
+
+
+]
